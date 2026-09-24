de2f55e91552e44736cf670ab860b1ae87f11611
lamonttay <ariqmuh110711@gmail.com>
2026-09-24 09:31:11 +0700

    second commit

--- app/src/main/java/com/example/mycourse/MainActivity.kt
+++ app/src/main/java/com/example/mycourse/MainActivity.kt
@@ -5,30 +5,32 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.viewpager2.widget.ViewPager2
 import com.example.mycourse.databinding.ActivityMainBinding
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val sectionsPagerAdapter = SectionsPagerAdapter(this)
-        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
 
-        viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = findViewById(R.id.tab_layout)
+        with(binding) {
+            viewPager.adapter = sectionsPagerAdapter
 
-        TabLayoutMediator(tabs, viewPager) { tab, position ->
-            tab.text = resources.getString(TAB_TITLES[position])
-        }.attach()
+            TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+                tab.text = resources.getString(TAB_TITLES[position])
+            }.attach()
+        }
+
         supportActionBar?.elevation = 0f
     }
 
@@ -36,24 +38,28 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.menu_options, menu)
         return true
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.action_home -> {
                 Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
                 true
             }
+
             R.id.action_materi -> {
-// Logika untuk melihat skor
                 Toast.makeText(this, "Materi", Toast.LENGTH_SHORT).show()
                 true
             }
+
             R.id.action_quiz -> {
                 Toast.makeText(this, "Quiz", Toast.LENGTH_SHORT).show()
                 true
             }
+
             else -> super.onOptionsItemSelected(item)
         }
     }
+
     companion object {
         @StringRes
         private val TAB_TITLES = intArrayOf(
@@ -61,5 +67,4 @@ class MainActivity : AppCompatActivity() {
             R.string.tab_text_2
         )
     }
-
 }
